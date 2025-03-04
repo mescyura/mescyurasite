5a6e3a06901262eb53a8dd44acc9faa3e3b3471b
YuriiHolyda <mescyura@gmail.com>
2025-03-04 12:45:42 +0200

    add tooltip for techitems

--- src/components/techItem/TechItem.tsx
+++ src/components/techItem/TechItem.tsx
@@ -1,4 +1,5 @@
 import { IconType } from 'react-icons';
+import { Tooltip } from 'react-tooltip';
 
 import classes from './TechItem.module.css';
 
@@ -7,10 +8,17 @@ interface TechProps {
 	icon: IconType;
 }
 
-export const TechItem = ({ icon }: TechProps) => {
+export const TechItem = ({ name, icon }: TechProps) => {
 	return (
-		<span className={classes.tech_icon_wrapper}>
-			{icon({ className: classes.tech_icon })}
-		</span>
+		<>
+			<span
+				className={classes.tech_icon_wrapper}
+				data-tooltip-id={name}
+				data-tooltip-content={name}
+			>
+				{icon({ className: classes.tech_icon })}
+			</span>
+			<Tooltip className={classes.tippy} id={name} />
+		</>
 	);
 };
