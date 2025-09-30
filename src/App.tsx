6813d8074df3423297989f5f8160b8e69c6867c6
mescyura <mescyura@gmail.com>
2025-09-30 14:19:45 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import { Translations } from './interfaces';
 
